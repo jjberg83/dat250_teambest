9d2f4f62114414f3f05174ad89538a578d7c930d
Jørund Jenssen Berg <jjberg@gmail.com>
2022-09-18 11:04:58 +0200

    Revert "Fjernet unødvendige felt fra Users table i SQLite"
    This reverts commit 1d40aa4b70782b885297d4fc416bb3f039647af1.

--- app/schema.sql
+++ app/schema.sql
@@ -17,7 +17,13 @@ CREATE TABLE [Users] (
   username VARCHAR,
   first_name VARCHAR,
   last_name VARCHAR,
-  [password] VARCHAR
+  [password] VARCHAR,
+  education VARCHAR DEFAULT 'Unknown',
+  employment VARCHAR DEFAULT 'Unknown',
+  music VARCHAR DEFAULT 'Unknown',
+  movie VARCHAR DEFAULT 'Unknown',
+  nationality VARCHAR DEFAULT 'Unknown',
+  birthday DATE DEFAULT 'Unknown'
 );
 
 -- ---
