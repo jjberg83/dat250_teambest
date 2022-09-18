6e779fbdd0cb762d77c9d4e3899bc6e0b563526c
Jørund Jenssen Berg <jjberg@gmail.com>
2022-09-18 05:43:34 +0200

    Importerer også Length i forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FormField, TextAreaField, FileField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 
 # defines all forms in the application, these will be instantiated by the template,
 # and the routes.py will read the values of the fields
